353afd6d4dd949ca3ab15861ef8f0664db0dbdbd
Teeranai K <moji_001122@hotmail.com>
2020-11-17 21:03:11 +0700

    🆕INIT

---
+++ classroom.py
@@ -0,0 +1,13 @@
+def startMenu():
+    start = input()
+    if (start == "0"):
+        # Go to register
+    elif (start == "1"):
+        # Go to sign in
+    else:
+        print("Please input a valid number\n0 for register\n1 for sign in")
+        startMenu()
+
+
+
+
