1c891d8e4b80f3117b61ac9440dd84b2663529a3
Teeranai K <moji_001122@hotmail.com>
2020-11-17 21:51:40 +0700

    🛠 Change info from list to dictionary

--- classroom.py
+++ classroom.py
@@ -1,14 +1,14 @@
-user = {}
+import time
+
+users = []
 
 
 def startMenu():
     print("Welcome to Classroom!")
     start = input("Please input...\n0 for register\n1 for sign in")
     if (start == "0"):
-        # Go to register
         register()
     elif (start == "1"):
-        # Go to sign in
         singIn()
     else:
         print("Invalid command")
@@ -17,22 +17,30 @@ def startMenu():
 
 def register():
     stdid = input("Student ID: ")
-    if (stdid in user):
-        print("User already exist")
-        register()
-    else:
-        info = list([input("Student Name: ")])
-        info.append(input("Student Surname: "))
-        info.append(input("Age: "))
-        info.append(input("School Name: "))
-        info.append(input("Year: "))
-        info.append(input("Telephone Number: "))
-        info.append(input("Course Name: "))
-        info.append(100)
-        info.append(input("Username: "))
-        info.append(input("Password: "))
-        info.append(input("Courses ID: ").split(sep=","))
-        print(info)
+    for user in users:
+        if user["Student ID"] == stdid:
+            print("User already exist\nExiting program")
+            for i in range(3):
+                print(".", end=" ")
+                time.sleep(0.5)
+                return null
+        else:
+            continue
+            
+    info = {
+        "Student ID" : stdid,
+        "Student Name" : input("Student Name: "),
+        "Student Surname" : input("Student Surname: "),
+        "Age": input("Age: "),
+        "School Name" : input("School Name: "),
+        "Year" : input("Year: ")
+        "Telephone Number" : input("Telephone Number: "),
+        "Course Name" : input("Course Name: "),
+        "Hour left" : 100,
+        "Username" : input("Username: "),
+        "Password" : input("Password: "),
+        "Courses ID" : input("Courses ID: ").split(sep=",")
+    }
 
 
-register()
+
