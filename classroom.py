fd6154223fb0ffd77e3288dd587e47b01b11ebe8
Teeranai K <moji_001122@hotmail.com>
2020-11-19 10:01:37 +0700

    ➕Register now creates an obj

--- classroom.py
+++ classroom.py
@@ -5,78 +5,99 @@ users = []
 
 def startMenu():
     print("Welcome to Classroom!")
-    start = input("Please input...\n0 for register\n1 for sign in")
+    start = input("Please input...\n0 for register\n1 for sign in\n")
     if (start == "0"):
-        register()
+        return register()
     elif (start == "1"):
-        singIn()
+        return signIn()
     else:
         print("Invalid command")
-        startMenu()
+        return startMenu()
 
 
 def register():
     stdid = input("Student ID: ")
     for user in users:
-        if user["Student ID"] == stdid:
+        if user.stdid == stdid:
             print("User already exist\nExiting program")
             for i in range(3):
                 print(".", end=" ")
                 time.sleep(0.5)
-                return
+                return startMenu()
         else:
             continue
-            
-    info = {
-        "Student ID" : stdid,
-        "Student Name" : input("Student Name: "),
-        "Student Surname" : input("Student Surname: "),
-        "Age": input("Age: "),
-        "School Name" : input("School Name: "),
-        "Year" : input("Year: "),
-        "Telephone": input("Telephone: "),
-        "Course Name" : input("Course Name: "),
-        "Hour left" : 100,
-        "Username" : input("Username: "),
-        "Password" : input("Password: "),
-        "Courses ID" : input("Courses ID: ").split(sep=",")
-    }
+    name = input("Student Name: ")
+    surname = input("Student Surname: ")
+    age = input("Age: ")
+    school = input("School Name: ")
+    year = input("Year: ")
+    phone = input("Telephone: ")
+    course = input("Course Name: ")
+    hour = 100
+    username = input("Username: ")
+    password = input("Password: ")
+    courses = input("Courses ID: ").split(sep=",")
 
-    users.append(info)
+    newUser = student(name, surname, age, stdid, school, year, phone, course, username, password, hour, courses)
 
-#student class for represent student
+    users.append(newUser)
+    return landingPage(newUser)
+
+def signIn():
+    enterUser = input("Username: ")
+    for user in users:
+        if (enterUser == user.username):
+            enterPw = input("Password: ")
+            if (enterPw == user.password):
+                print("Sign in success")
+                time.sleep(1)
+                return landingPage(user)
+            else:
+                print("Invalid password")
+                return signIn()
+        print("User not found")
+        return signIn()
+                
+def landingPage(user):
+    user.printInfo()
+
 class student:
-  def __init__(self, name, sur, age, Id, school, year, userName, passWord, timeleft):
-    self.name = name
-    self.age = age
-    self.sur = sur
-    self.age = age
-    self.Id = Id
-    self.school = school
-    self.year = year
-    self.userName = userName
-    self.passWord = passWord
-    self.timeleft = timeleft
-    
+    def __init__(self, name, surname, age, stdid, school, year, phone, course, username, password, timeleft, courses):
+        self.name = name
+        self.age = age
+        self.surname = surname
+        self.age = age
+        self.stdid = stdid
+        self.school = school
+        self.year = year
+        self.username = username
+        self.password = password
+        self.timeleft = timeleft
+        self.phone = phone
+        self.course = course
+        self.courses = courses
 
-  def printInfo(self):
-    print("Name: "+ self.name)
-    print("Surname: "+ self.sur)
-    print("Age: "+ str(self.age))
-    print("student ID: "+ str(self.Id))
-    print("School: "+ self.school)
-    print("year: "+ str(self.year))
-    print("username: "+ self.userName)
-    print("timeleft: "+str(self.timeleft))
+    def printInfo(self):
+        print("\n\n\n")
+        print("Welcome, " + self.name)
+        print("Surname: " + self.surname)
+        print("Age: " + str(self.age))
+        print("student ID: " + str(self.stdid))
+        print("School: " + self.school)
+        print("year: " + str(self.year))
+        print("username: " + self.username)
+        print("timeleft: "+str(self.timeleft))
+        print("Courses: "+str(self.courses))
 
-#classroom 100 seats
+# classroom 100 seats
+
+
 class classRoom:
     def __init__(self, seats):
         self.seats = 100
-    #Seat will decrease when has student reserve 
+    # Seat will decrease when has student reserve
 
     def classRoomInfo(self):
         print(str(self.seats))
-    
-    
 
+startMenu()
