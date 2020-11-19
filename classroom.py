0f30f257d83122b5d39df94988c25f9197654617
Teeranai K <moji_001122@hotmail.com>
2020-11-19 12:41:07 +0700

    ➕Store,get user in file

--- classroom.py
+++ classroom.py
@@ -2,8 +2,15 @@ import time
 
 users = []
 
+def programInit():
+    userdata = open("userdata.txt")
+    for line in userdata:
+        info = line.split(sep=",")
+        std = student(info[0],info[1],info[2],info[3],info[4],info[5],info[6],info[7],info[8],info[9],info[10],info[11])
+        users.append(std)
 
 def startMenu():
+    programInit()
     print("Welcome to Classroom!")
     start = input("Please input...\n0 for register\n1 for sign in\n")
     if (start == "0"):
@@ -16,39 +23,50 @@ def startMenu():
 
 
 def register():
-    stdid = input("Student ID: ")
+    username = input("Username: ")   
     for user in users:
-        if user.stdid == stdid:
+        print(getattr(user, 'username'))
+        if getattr(user, 'username') == username:
             print("User already exist\nExiting program")
-            for i in range(3):
+            for i in range(4):
                 print(".", end=" ")
-                time.sleep(0.5)
-                return startMenu()
+                time.sleep(1)
+            exit()
         else:
             continue
+    password = input("Password: ")
     name = input("Student Name: ")
     surname = input("Student Surname: ")
+    stdid = input("Student ID: ")
     age = input("Age: ")
     school = input("School Name: ")
     year = input("Year: ")
     phone = input("Telephone: ")
     course = input("Course Name: ")
     hour = 100
-    username = input("Username: ")
-    password = input("Password: ")
-    courses = input("Courses ID: ").split(sep=",")
+    courseID = input("Course ID: ")
 
-    newUser = student(name, surname, age, stdid, school, year, phone, course, username, password, hour, courses)
-
+    newUser = student(name, surname, age, stdid, school, year, phone, course, username, password, hour, courseID)
+    addTo = open("userdata.txt", "a")
+    addTo.write(name+","+surname+","+age+","+stdid+","+school+","+year+","+phone+","+course+","+username+","+password+","+str(hour)+","+courseID+"\n")
     users.append(newUser)
-    return landingPage(newUser)
+    addTo.close()
+    print("Add more user?\n")
+    ex = input("1 to add more\n2 to go back to start menu\n")
+    if (ex == "1"):
+        return register()
+    elif (ex == "2"):
+        return startMenu()
+    else: exit()
+
+    
 
 def signIn():
     enterUser = input("Username: ")
     for user in users:
-        if (enterUser == user.username):
+        if (enterUser == getattr(user, 'username')):
             enterPw = input("Password: ")
-            if (enterPw == user.password):
+            if (enterPw == getattr(user, 'password')):
                 print("Sign in success")
                 time.sleep(1)
                 return landingPage(user)
@@ -62,7 +80,7 @@ def landingPage(user):
     user.printInfo()
 
 class student:
-    def __init__(self, name, surname, age, stdid, school, year, phone, course, username, password, timeleft, courses):
+    def __init__(self, name, surname, age, stdid, school, year, phone, course, username, password, timeleft, courseID):
         self.name = name
         self.age = age
         self.surname = surname
@@ -75,19 +93,18 @@ class student:
         self.timeleft = timeleft
         self.phone = phone
         self.course = course
-        self.courses = courses
+        self.courseID = courseID
 
     def printInfo(self):
-        print("\n\n\n")
-        print("Welcome, " + self.name)
-        print("Surname: " + self.surname)
+        print("\n\n")
+        print("Welcome, ",self.name,self.surname)
         print("Age: " + str(self.age))
         print("student ID: " + str(self.stdid))
         print("School: " + self.school)
         print("year: " + str(self.year))
         print("username: " + self.username)
         print("timeleft: "+str(self.timeleft))
-        print("Courses: "+str(self.courses))
+        print("Course ID: "+str(self.courseID))
 
 # classroom 100 seats
 
